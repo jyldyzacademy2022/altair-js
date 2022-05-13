ec6ee46c53852ea17f6d2cc6d14f5b6040720216
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-05-13 22:53:29 +0600

    Add lesson 13-05-22

---
+++ Lesson-13-05-22/script.js
@@ -0,0 +1,50 @@
+// "use strict"
+// Single line comment
+/*
+  Multiline
+  comment
+*/
+
+// Identifier
+// var user;
+// user = "Tashmat";
+// user = "Ivan";
+
+// var user = "Tashmat";
+// user = "Eshmat";
+
+// let user name = "20=";
+// let num2 = 3;
+
+// const myPi = 3.14;
+// myPi = "Hello";
+
+// let firstName = "john";
+// let lastName = "doe";
+
+// let fullName = firstName / lastName;
+
+// let speed = parseInt(prompt("Кандай ылдамдыкта журдунуз?"));
+// let time = parseInt(prompt("Канча убакыт журдунуз?"));
+
+// let result = speed * time;
+
+// console.log(result);
+// document.write(result);
+
+// 2 + 2 * 3
+
+// let a = "1";
+// let b = "2";
+
+// a = parseFloat(a);
+// b = parseFloat(b);
+// let c = a + b;
+
+// console.log(c);
+// console.log(typeof c);
+
+let myStr = "40.asdf3sadf0asd;4f";
+myStr = parseFloat(myStr);
+console.log(myStr);
+console.log(typeof myStr);
