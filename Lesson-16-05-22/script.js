455889c661c37118461ec1c3b3b535030b9797af
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-05-16 22:29:12 +0600

    Add Lesson 16-05-22

---
+++ Lesson-16-05-22/script.js
@@ -0,0 +1,41 @@
+// let a = 2;
+// let b = 2;
+
+// a = a + b;
+// a += b;
+
+// let d;
+
+// console.log(d);
+
+// 0123456789
+// let userPasswordLength = userPassword.length;
+
+// let userName = "Pete";
+// // let greeting = "Hello " + userName;
+// let greeting = `Hello ${userName}`;
+
+// console.log(greeting);
+
+// null
+// undefined
+// string
+// number
+// boolean: true, false
+
+// let userName = "John";
+// let userAge = 30;
+// let isMarried = true;
+
+let user = {
+  userName: "Mary",
+  age: 30,
+  isMarried: true,
+  hasCar: null,
+};
+
+// let loggedUser = user.userName;
+
+// console.log(user.userName);
+console.log(user.userName);
+// console.log(user.age);
