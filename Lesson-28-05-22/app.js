07c7f937658b79735275f8d8f194b26e89770bba
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-06-03 20:49:10 +0600

    Add lesson May 28

---
+++ Lesson-28-05-22/app.js
@@ -0,0 +1,99 @@
+// let user = prompt("What is your name?");
+
+// function greet(userName) {
+//   alert("Hello " + userName);
+// }
+
+// greet(user);
+// greet("Erlan");
+
+// function calcSum(num1, num2) {
+//   document.write(num1 + num2);
+// }
+
+// calcSum(10, 10);
+// document.write("<br>");
+// calcSum(100, 150);
+
+// let myStr = "hello";
+
+// function calcSum(num1, num2) {
+// let result = num1 + num2;
+
+// if (typeof num1 === "number" && typeof num2 === "number") {
+//   return num1 + num2;
+// } else {
+//   // console.log("Check your args!");
+//   console.error("wrong args types");
+//   // return "typeof arg1: " + typeof num1 + " typeof arg2: " + typeof num2;
+// }
+
+// console.log(myStr);
+// }
+
+// calcSum();
+
+// console.log(calcSum(10, "hello"));
+// console.log(calcSum(100, 10));
+
+// console.log(typeof 5);
+
+// console.log(result);
+
+// function greet(userName = "Guest") {
+//   document.write(`Hello ${userName}`);
+// }
+
+// greet();
+// greet("Masha");
+
+// console.log(calcSum(500, 500));
+// function calcSum(a, b) {
+//   return a + b;
+// }
+
+// let result = calcSum(20, 20);
+
+// console.log(result);
+// document.write(result);
+
+// console.log(user);
+
+// const user = "john";
+
+// const calcSum = function (a, b) {
+//   return a + b;
+// };
+
+// console.log(calcSum(20, 30));
+
+// calcSum = 50;
+
+// console.log(calcSum(40, 40));
+
+// const sum = (a) => {
+//   return a;
+// };
+
+// console.log(sum(500));
+
+// function () {
+//   console.log("hello")
+// }
+
+// "10" ==> 10
+
+// yourfunc(45)
+
+// console.log(false)
+
+// yourfunc('Mirlan', ['john', 'masha', 'peter'])
+// console.log(false)
+
+// console.log(5 % 2 === 1);
+
+if (5 % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
