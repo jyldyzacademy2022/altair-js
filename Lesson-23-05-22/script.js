da0ab69bf5f90d563162454c5b8d2bd2d9059118
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-06-03 20:48:50 +0600

    Add lesson may 23

---
+++ Lesson-23-05-22/script.js
@@ -0,0 +1,70 @@
+// Comparison
+// > --> GT
+// < --> LT
+// == --> Equal
+// === --> EQ
+// >= --> GT and EQ
+// <=
+// != --> Not EQ
+
+// let myNum = "10";
+
+// console.log(myNum == 10);
+
+// FALSY VALUES
+
+/*
+
+false
+0
+-0
+""
+null
+undefined
+NaN
+
+*/
+
+// console.log(-1 == false);
+
+// TRUTHY
+
+// LOGICAL
+/*
+  && --> AND
+  || --> OR
+  ! --> NOT
+*/
+
+// let shop = "close";
+// let money = 115;
+
+// console.log(!false);
+
+// let hour = 4;
+
+// if (hour >= 5 && hour <= 10) {
+//   console.log("Kutman Tan!");
+// } else if (hour > 10 && hour <= 18) {
+//   console.log("Kutman Kun!");
+// } else if (hour > 18 && hour <= 22) {
+//   console.log("Kutman Kech!");
+// } else {
+//   console.log("Kutman Tun!");
+// }
+
+let user = "azamat";
+
+// if (user) {
+//   console.log("Hello " + user);
+// } else {
+//   console.log("I don't know who you are!");
+// }
+
+// Ternary
+// условие ? выражение1 : выражение2
+user == "azamat"
+  ? console.log("Hello " + user)
+  : console.log("I don't know who you are!");
+
+// CTRL + R
