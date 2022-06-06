b860df46428fdc69c2513516205342e8d249b2d1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-06-06 21:39:57 +0600

    Add lesson code

---
+++ June/Lesson-06-06-22/app.js
@@ -0,0 +1,16 @@
+// const title = document.querySelector("#title");
+// const titles = document.querySelectorAll("h2.title");
+
+// console.log(titles);
+// console.log(title);
+
+// селектор по тегу: p, section
+// селектор по классу: .title
+// селектор по id: #id
+// селектор по атрибуту: [type='text']
+// вложенные селекторы: section h1 span
+
+// const title = document.getElementById("title");
+// const titlesByClass = document.getElementsByClassName("title");
+// const mainTitle = document.getElementsByTagName("h1");
+// console.log(mainTitle);
