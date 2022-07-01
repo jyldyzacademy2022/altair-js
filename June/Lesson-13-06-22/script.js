6ae3b73a16e2f985ba4040594b78dc97874ad0c1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-07-01 22:47:02 +0600

    update lessons

---
+++ June/Lesson-13-06-22/script.js
@@ -0,0 +1,113 @@
+// var name;
+// name = "John";
+// name = "Pete";
+// let userName;
+// userName = "Masha";
+// userName = "Tashmat";
+
+// const pi = 315;
+
+// // start with A-Z a-z
+// // a-z12434
+// // $userName
+// // user_name
+// // user != USER
+// // userName
+// // USER_NAME
+
+// console.log(pi);
+
+// Primitive
+// number
+// let age = 29;
+// let price = 45.12;
+
+// string
+// let myStr = "John";
+// let myStr3 = `My name is ${45 + 50}`;
+
+// let firstName = "    john    ";
+// // let lastName = "Doe   ";
+// // let fullName = firstName.concat(" ", lastName); // john Doe
+// // //  0123456...
+
+// // let fullNameTrimmed = fullName.trim();
+// // console.log(firstName.trim());
+
+// // let user = "john";
+
+// // let userName = user[0].toLocaleUpperCase() + user.slice(1);
+
+// let fullName = "john doe smith";
+// const h1 = document.createElement("h1");
+// h1.innerText = fullName;
+// h1.style.textTransform = "capitalize";
+
+// document.body.appendChild(h1);
+
+// // console.log(`Hello, ${}`);
+
+// console.log(($("h1").innerText = "jquery"));
+
+// math operators
+// let a = 5;
+// let b = 10;
+// console.log(a % b);
+
+// +
+// let userInput = +prompt("Enter a number");
+// let userInput2 = +prompt("Enter another number");
+
+// console.log(userInput + userInput2);
+
+// increment
+// let x = 10;
+// x++;
+// console.log(x);
+
+// // decrement
+// let y = 10;
+// y--;
+// console.log(y);
+
+// let x = 10;
+// x += 2; // x = x + 2
+// x -= 10;
+// x *= 2;
+// x /= 2;
+// console.log(x);
+
+// 2 + (2 - 2)
+
+// >
+// console.log(4 === "4");
+
+// ||, &&, !
+// console.log(!true);
+
+let userRole = "admin";
+
+// if (userRole === "admin") {
+//   console.log("hello admin!");
+// } else if (userRole === "moderator") {
+//   console.log("hello moderator");
+// } else {
+//   console.log("hello guest");
+// }
+
+// userRole === "admin" ? console.log("hello admin") : console.log("hello guest");
+
+switch (userRole) {
+  case "admin":
+    console.log("hello admin");
+    break;
+  case "moderator":
+    console.log("hello moderator");
+    break;
+  default:
+    console.log("hello guest");
+}
+
+// ссылочные типы
+object;
+massive;
